b1b0a85a06234eaea675ad4b67bb3a130f1796c8
thummim <35131165+thummim@users.noreply.github.com>
2022-03-26 21:57:24 +0900

    Update Baekjoon_Q10988.cpp

--- Baekjoon/Baekjoon_Q10988.cpp
+++ Baekjoon/Baekjoon_Q10988.cpp
@@ -4,12 +4,12 @@ string s, s2;
 
 int main()
 {
-    cin >> s;
+    cin >> s; // 문자열 입력 받음
     
-    s2 = s;
+    s2 = s; // 입력 받은 문자열을 뒤집어서 비교하기 위해 다른 문자열에 넣음
     
-    reverse(s2.begin(), s2.end());
+    reverse(s2.begin(), s2.end()); // reverse로 string 배열을 뒤집음
  
-    if(s == s2) cout << "1";
-    else cout << "0";
+    if(s == s2) cout << "1"; // 거꾸로 읽어도 똑같으면 팰린드롬이므로 1을 출력
+    else cout << "0"; // 팰린드롬이 아니라면 0을 출력
 }
