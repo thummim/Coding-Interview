4df5d58e89ecf330404d25d39641ef950b0e60a1
thummim <35131165+thummim@users.noreply.github.com>
2022-03-29 15:00:51 +0900

    Create Baekjoon_Q18108.cpp

---
+++ Baekjoon/단계별로 풀어보기/입출력과 사칙연산/Baekjoon_Q18108.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, ret;
+
+int main()
+{
+    cin >> a; //서기연도 입력
+    
+    ret = a - 543; // 543을 빼주어서 불기 연도로 만들어줌
+    
+    cout << ret << "\n"; 
+    
+    return 0;
+}
