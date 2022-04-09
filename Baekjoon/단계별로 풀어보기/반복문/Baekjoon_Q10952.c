d3b120553b73109648423b9bb89569e138970781
thummim <35131165+thummim@users.noreply.github.com>
2022-04-09 17:01:56 +0900

    Create Baekjoon_Q10952.c

---
+++ Baekjoon/단계별로 풀어보기/반복문/Baekjoon_Q10952.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 0;
+    int B = 0;
+    
+    while (1)
+    {
+        scanf("%d %d", &A, &B);
+        if(A == 0 && B == 0) break;
+        printf("%d\n", A+B);
+    }
+    
+    return 0;
+}
